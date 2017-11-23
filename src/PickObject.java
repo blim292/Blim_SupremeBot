6f5cd5fe5b6d892ea082b1a797b70c8eda5e5cf6
Brian Lim <blim292@gmail.com>
2017-11-23 11:42:03 -0800

    Initial commit

---
+++ src/PickObject.java
@@ -0,0 +1,135 @@
+import java.util.Scanner;
+
+public class PickObject {
+	String itemName, itemSize, itemColor;
+	String category;
+	String name, email, tele, address, apt, city, state, country;
+	String zip = "";
+	String ccNum, ccMonth, ccYear, ccv;
+	
+	//	Eventually use GUI for user selection.
+	void pickItem() {
+		Scanner kb = new Scanner(System.in);
+		
+//		System.out.println("Please provide your billing/shipping info below.");
+//		System.out.print("Name: ");
+//		name = kb.nextLine();
+//		System.out.print("Email: ");
+//		email = kb.nextLine();
+//		System.out.print("Telephone Number: ");
+//		tele = kb.nextLine();
+//		System.out.print("Address: ");
+//		address = kb.nextLine();
+//		
+//		String tempConfirm;
+//		do {
+//			System.out.println("Do you have an apartment unit? (Y/N): ");
+//			tempConfirm = kb.nextLine();
+//		}
+//		while (!tempConfirm.equals("Y") && !tempConfirm.equals("N"));
+//		
+//		if (tempConfirm.equals("Y")) {
+//			System.out.print("Apt:");
+//			apt = kb.nextLine();
+//		}
+//		
+//		System.out.print("ZIP: ");
+//		zip = kb.nextLine();
+//		System.out.print("City: ");
+//		city = kb.nextLine();
+//
+//		System.out.print("State: ");
+//		state = kb.nextLine();
+//		do {
+//			System.out.print("Country (USA or Canada): ");
+//			tempConfirm = kb.nextLine();
+//			tempConfirm = tempConfirm.toUpperCase();
+//		}
+//		while (!tempConfirm.equals("USA") && !tempConfirm.equals("CANADA"));
+//		country = tempConfirm;
+//		
+//		System.out.println("Please enter your cc info below.");
+//		System.out.print("Number: ");
+//		ccNum = kb.nextLine();
+//		System.out.print("CCV: ");
+//		ccv = kb.nextLine();
+//		System.out.print("CC Month: ");
+//		ccMonth = kb.nextLine();
+//		System.out.print("CC Year: ");
+//		ccYear = kb.nextLine();
+		
+		
+		int userChoice;
+		do {
+			System.out.println("Pick the number that is associated with the product you are looking for.");
+			System.out.println("1. Jackets");
+			System.out.println("2. Shirts");
+			System.out.println("3. Tops/Sweaters");
+			System.out.println("4. Sweatshirts");
+			System.out.println("5. Pants");
+			System.out.println("6. T-shirts");
+			System.out.println("7. Hats");
+			System.out.println("8. Accessories");
+			System.out.println("9. Skate");
+			
+			System.out.print("> ");
+			userChoice = Integer.parseInt(kb.nextLine());
+			System.out.println("You entered " + userChoice);
+		}
+		while (userChoice < 1 || userChoice > 8);
+		
+
+		switch (userChoice) {
+		case 1:
+			category = "/shop/all/jackets";
+			break;
+		case 2:
+			category = "shop/all/shirts";
+			break;
+		case 3:
+			category = "/shop/all/tops_sweaters";
+			break;
+		case 4:
+			category = "shop/all/sweatshirts";
+			break;
+		case 5:
+			category = "/shop/all/pants";
+			break;
+		case 6:
+			category = "/shop/all/t-shirts";
+			break;
+		case 7:
+			category = "/shop/all/hats";
+			break;
+		case 8:
+			category = "/shop/all/accessories";
+			break;
+		case 9:
+			category = "/shop/all/skate";
+			break;
+		default:
+			System.out.println("Something went wrong with the input.");
+			category = "";
+			break;
+		}	
+		
+		System.out.println("What is the name of the item you would like to purchase?");
+		System.out.print("> ");
+		itemName = kb.nextLine();
+
+		String isSize;
+		do {
+			System.out.println("What size would you like to purchase?");
+			System.out.print("> ");
+			isSize = kb.nextLine();
+		}
+		while (!isSize.equals("XS") && !isSize.equals("S") && !isSize.equals("M") && !isSize.equals("L") && !isSize.equals("XL"));
+		itemSize = isSize;
+		
+		System.out.println("What color would you like to purchase?");
+		System.out.print("> ");
+		itemColor = kb.nextLine();
+		kb.close();
+	}
+	
+}
---
+++ src/SeleniumTest.java
@@ -0,0 +1,129 @@
+
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SeleniumTest {
+
+	public static void main(String[] args) {
+		
+		PickObject userObj = new PickObject();
+		userObj.pickItem();
+
+		String supremeSite = "http://www.supremenewyork.com";
+
+		
+		String projectLocation = System.getProperty("user.dir");
+		System.setProperty("webdriver.chrome.driver", projectLocation + "/lib/chromedriver/chromedriver");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get(supremeSite + userObj.category);
+		
+//		Page refresher.
+		do{
+			int randomNum = ThreadLocalRandom.current().nextInt(800, 1200 + 1); // between .8 - 1.2 second refresh rate.
+			try {
+				Thread.sleep(randomNum);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			driver.navigate().refresh();
+		}
+		while(!driver.getPageSource().contains(userObj.itemName));
+		
+	
+		if (driver.getPageSource().contains(userObj.itemName)) {
+			
+//			Check to make sure the product name and color point to the same link.
+//			If both point to same link, go to link.
+//			List<WebElement> itemHref = driver.findElements(By.partialLinkText(userObj.itemName));
+//			List<WebElement> colorHref = driver.findElements(By.partialLinkText(userObj.itemColor));
+//			
+//			for(WebElement weItem : itemHref) {
+//				for (WebElement weColor : colorHref) {
+//					if (weItem.getAttribute("href").equals(weColor.getAttribute("href"))) {
+//						driver.get(weItem.getAttribute("href"));
+//						break;
+//					}
+//				}
+//			}
+			driver.findElement(By.partialLinkText(userObj.itemName)).click();
+			
+//			Change Size
+			WebDriverWait wait1 = new WebDriverWait(driver,120);
+//			wait1.until(ExpectedConditions.elementToBeClickable(By.name("s")));
+//			
+//			Select droplist = new Select(driver.findElement(By.name("s")));   
+//			
+//			String cartSize;
+//			switch (userObj.itemSize) {
+//			case "XS":
+//				cartSize = "XSmall";
+//				break;
+//			case "S":
+//				cartSize = "Small";
+//				break;
+//			case "M":
+//				cartSize = "Medium";
+//				break;
+//			case "L":
+//				cartSize = "Large";
+//				break;
+//			case "XL":
+//				cartSize = "XLarge";
+//				break;
+//			default:
+//				cartSize = "Small";
+//				break;
+//			}
+//			
+//			droplist.selectByVisibleText(cartSize);
+			
+//			Cart item.
+			wait1.until(ExpectedConditions.elementToBeClickable(By.name("commit")));
+			driver.findElement(By.name("commit")).click();
+			wait1.until(ExpectedConditions.visibilityOfElementLocated(By.partialLinkText("checkout")));
+			driver.findElement(By.partialLinkText("checkout")).click();
+			
+//			Checkout.
+			JavascriptExecutor js = (JavascriptExecutor) driver;
+			js.executeScript("document.getElementById('order_billing_name').value = arguments[0]", userObj.name);
+			js.executeScript("document.getElementById('order_email').value = arguments[0]", userObj.email);
+			js.executeScript("document.getElementById('order_tel').value = arguments[0]", userObj.tele);
+			js.executeScript("document.getElementById('bo').value = arguments[0]", userObj.address);
+			js.executeScript("document.getElementById('order_billing_zip').value = arguments[0]", userObj.zip);
+			js.executeScript("document.getElementById('order_billing_city').value = arguments[0]", userObj.city);
+			js.executeScript("document.getElementById('order_billing_state').value = arguments[0]", userObj.state);
+			js.executeScript("document.getElementById('order_billing_country').value = arguments[0]", userObj.country);
+			js.executeScript("document.getElementById('nnaerb').value = arguments[0]", userObj.ccNum);
+			js.executeScript("document.getElementById('credit_card_month').value = arguments[0]", userObj.ccMonth);
+			js.executeScript("document.getElementById('credit_card_year').value = arguments[0]", userObj.ccYear);
+			js.executeScript("document.getElementById('orcer').value = arguments[0]", userObj.ccv);
+						
+			WebElement element = driver.findElement(By.id("order_terms"));
+			Actions actions = new Actions(driver);
+			actions.moveToElement(element).click().perform();
+
+//			driver.findElement(By.name("commit")).click();
+			
+			
+		}
+		else {
+			System.out.println("String you are searching for not present on webpage.");
+		}
+		
+//		driver.quit();
+	}
+
+}
